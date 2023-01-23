78cc561a15a27a8a35f374ddf215af255f114541
Santiago Sierra Herrera <santiago.sierra@sofka.com.co>
2023-01-23 15:35:58 -0500

    feat:left option pannel completed

--- src/app/components/molecules/molecules.module.ts
+++ src/app/components/molecules/molecules.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AtomsModule } from '../atoms/atoms.module';
 import { LeftOptionComponent } from './left-option/left-option.component';
+import { RightOptionComponent } from './right-option/right-option.component';
 
 
 
 @NgModule({
-  declarations: [
-    LeftOptionComponent
-  ],
-  imports: [
-    CommonModule,
-    AtomsModule,
-    
-  ],
-  exports: [
-    LeftOptionComponent
-  ]
-
+    declarations: [
+        LeftOptionComponent,
+        RightOptionComponent
+    ],
+    exports: [
+        LeftOptionComponent
+    ],
+    imports: [
+        CommonModule,
+        AtomsModule,
+    ]
 })
 export class MoleculesModule { }
---
+++ src/app/components/molecules/right-option/right-option.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-option',
+  templateUrl: './right-option.component.html',
+  styleUrls: ['./right-option.component.css']
+})
+export class RightOptionComponent {
+
+}
